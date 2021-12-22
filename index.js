2d779f9094ea9549fa33d04f38a3e53a04c036fa
Andrew LeRoux <andrewleroux2639@gmail.com>
2021-12-22 10:42:17 -0800

    commit tree 4b0691a54857bff49a6dd9500c297d44dff70ab6

--- index.js
+++ index.js
@@ -1,4 +1,66 @@
-/* Your Code Here */
+function createEmployeeRecord(array){
+    return {
+        'firstName': array[0],
+        'familyName': array[1],
+        'title': array[2],
+        'payPerHour': array[3],
+        'timeInEvents': [],
+        'timeOutEvents': []
+    }
+}
+
+function createEmployeeRecords(array) {
+    const newArray = []
+for (const arr of array) {
+    newArray.push(createEmployeeRecord(arr))
+}
+return newArray
+}
+
+
+function createTimeInEvent(dateStamp){
+    
+    this.timeInEvents.push({
+        'type': "TimeIn",
+        'hour': parseInt(dateStamp.slice(11)),
+        'date': dateStamp.slice(0,10)
+    })
+
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+
+    this.timeOutEvents.push({
+        'type': "TimeOut",
+        'hour': parseInt(dateStamp.slice(11)),
+        'date': dateStamp.slice(0,10)
+    })
+
+    return this
+}
+
+function hoursWorkedOnDate(inputDate){
+    let timeInHour = 0
+    let timeOutHour = 0
+    for (const obj of this.timeInEvents){
+        if (obj.date === inputDate) {
+            timeInHour = obj.hour / 100
+        }
+    }
+    for (const obj of this.timeOutEvents){
+        if (obj.date === inputDate) {
+            timeOutHour = obj.hour / 100
+        }
+    }
+    return timeOutHour - timeInHour
+
+}
+
+function wagesEarnedOnDate(inputDate){
+return hoursWorkedOnDate.call(this, inputDate) * this.payPerHour
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +83,19 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstName){
+    for (const arr of srcArray){
+        if (arr.firstName === firstName){
+            return arr
+        }
+    }
+}
+
+
+function calculatePayroll(array){
+    let totalPayrollValue = 0
+    for (let i = 0; i < array.length; i++){
+        totalPayrollValue = totalPayrollValue + allWagesFor.call(array[i])
+    }
+    return totalPayrollValue
+}
